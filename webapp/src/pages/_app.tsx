cd95ef38713fc40fad4aca8752ddb74d35a57553
codyx <codyx@users.noreply.github.com>
2024-05-19 10:56:17 +0200

    Rebranding

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -7,7 +7,7 @@ import type { AppProps /*, AppContext */ } from 'next/app'
 function Home({ Component, pageProps }: AppProps) {
   return <>
       <div className="home">
-      <h1>ProgramTracer - Fetch the source code of any program hash</h1>
+      <h1>Clarity - Fetch the source code of any Cairo program</h1>
       <Component {...pageProps} />
     </div>
   </>
