cb428e8c5065a9cd715e2710b4d6a0fe00cf8152
Bartosz Nowak <bartekn337@gmail.com>
2024-05-19 10:34:16 +0200

    axios localhost fix

--- webapp/src/pages/api/search.ts
+++ webapp/src/pages/api/search.ts
@@ -4,7 +4,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { q } = req.query;
 
-  const response = await axios.get(`http://localhost:4000/search?program_hash=${q}`);
+  const response = await axios.get(`http://127.0.0.1:4000/search`, {
+    params: { program_hash: q }
+  });
 
   if (!response || response.status === 404) {
     return res.status(200).json({ results: [] });
